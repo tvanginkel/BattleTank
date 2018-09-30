6a6fdc44fb2374353e264170e3056f037783bfbf
Toni <tginkel1@gmail.com>
2018-09-30 13:25:32 -0400

    Level with flat shadow created and tank created and placed in map, with no movement whatsoever.

---
+++ BattleTank_UE4/Source/BattleTank_UE4/BattleTank_UE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank_UE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank_UE4, "BattleTank_UE4" );
---
+++ BattleTank_UE4/Source/BattleTank_UE4/BattleTank_UE4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_UE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_UE4_API ABattleTank_UE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
