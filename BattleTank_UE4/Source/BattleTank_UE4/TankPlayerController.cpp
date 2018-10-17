0fc5768802da492e524455180a74640251fcfe2f
Toni <tginkel1@gmail.com>
2018-10-17 18:22:31 -0400

    Created first C++ classes
    -Tank.cpp
    -Tank.h
    -TankPlayerController.cpp
    -TankPlayerController.h
    
    -BeginPlay()
    -GetPlayerController()
    
    Udemy - Lecture 129

---
+++ BattleTank_UE4/Source/BattleTank_UE4/TankPlayerController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void  ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Begin Play"));
+
+	if (!GetControllerTank())
+		UE_LOG(LogTemp, Error, TEXT("TankPlayerController pawn not found"))
+	else
+		UE_LOG(LogTemp, Warning, TEXT("The name of the tank is: %s"), *(GetControllerTank()->GetName()))
+	
+}
+
+ATank* ATankPlayerController::GetControllerTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
---
+++ BattleTank_UE4/Source/BattleTank_UE4/TankPlayerController.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Public\Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" //Must be the last include
+
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_UE4_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATank* GetControllerTank() const;
+
+	void BeginPlay() override;
+	
+
+};
